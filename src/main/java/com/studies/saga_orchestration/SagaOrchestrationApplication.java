1b4b4a7e56af11549dc6a05334c4d2acee7b3076
Sophia_Wermann <sophia.wermann@dell.com>
2025-01-13 17:17:49 -0300

    First commit

---
+++ src/main/java/com/studies/saga_orchestration/SagaOrchestrationApplication.java
@@ -0,0 +1,13 @@
+package com.studies.saga_orchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaOrchestrationApplication.class, args);
+	}
+
+}
