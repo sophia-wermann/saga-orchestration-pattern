c115d39ad15090d70cb369d098fb5bfeff482db3
Sophia_Wermann <sophia.wermann@dell.com>
2025-02-03 23:34:50 -0300

    Product Validation: service

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Event.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Event.java
@@ -7,7 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -16,12 +19,19 @@ import java.util.List;
 public class Event {
 
     private String id;
-    private String transactionId;
     private String orderId;
+    private String transactionId;
     private Order payload;
     private String source;
     private ESagaStatus status;
     private List<History> eventHistory;
     private LocalDateTime createdAt;
+
+    public void addToHistory(History history) {
+        if (isEmpty(eventHistory)) {
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 
 }
